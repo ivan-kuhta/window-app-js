535feb16ffbe514795b3c37f2bbaf28439040d8b
Ivan <Ivan1998pi22342@ukr.net>
2020-07-16 18:07:32 +0300

    Add Tabs && Calculation

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,6 +1,10 @@
-const forms = () => {
+import checkNumImputs from './checkNumInputs';
+
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
-        input = document.querySelectorAll('input');
+        inputs = document.querySelectorAll('input');
+
+    checkNumImputs('input[name="user_phone"]')
 
     const message = {
         loading: 'Загрузка',
@@ -8,10 +12,49 @@ const forms = () => {
         failure: 'Что-то пошло не так...'
     };
 
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        })
+    }
+
     form.forEach(item => {
         item.addEventListener('submit', (e) => {
             e.preventDefault();
-            
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+            if(item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                }).catch(() => {
+                    statusMessage.textContent = message.failure;
+                }).finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                })
         })
     });
 
